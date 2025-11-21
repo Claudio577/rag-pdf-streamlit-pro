e095a2c7aaca32e059a8f4ec4f35f4a7f2665b34
Claudio577 <chideki.yoshida@gmail.com>
2025-11-21 08:02:06 -0300

    Create embeddings.py

---
+++ src/embeddings.py
@@ -0,0 +1,6 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def get_embeddings():
+    return HuggingFaceEmbeddings(
+        model_name="intfloat/multilingual-e5-base"
+    )
