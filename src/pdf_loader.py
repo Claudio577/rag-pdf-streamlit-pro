da8ae9ca23c5737dead662fbd7e42e775bea47bb
Claudio577 <chideki.yoshida@gmail.com>
2025-11-21 09:01:36 -0300

    Update pdf_loader.py

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,21 +1,28 @@
 import tempfile
+import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from src.preprocess import remove_governo_headers, clean_text_block
 from src.vectorstore import build_vectorstore
 
-import streamlit as st
-
 @st.cache_resource
-def load_and_index_pdfs(uploaded_files):
+def load_and_index_pdfs(pdf_bytes_list):
+    """
+    Recebe uma lista de bytes de PDFs.
+    Cria arquivos temporários.
+    Extrai texto.
+    Constrói um vectorstore FAISS.
+    """
 
     all_docs = []
 
-    for pdf in uploaded_files:
-        # Criar arquivo temp
+    for i, pdf_bytes in enumerate(pdf_bytes_list):
+        # Criar PDF temporário a partir de bytes
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
-            tmp.write(pdf.getbuffer())
+            tmp.write(pdf_bytes)
             temp_path = tmp.name
+
+        pdf_name = f"PDF_{i+1}.pdf"
 
         loader = PyPDFLoader(temp_path)
         raw_docs = loader.load()
@@ -30,12 +37,12 @@ def load_and_index_pdfs(uploaded_files):
             texto = remove_governo_headers(d.page_content)
             texto = clean_text_block(texto)
             d.page_content = "passage: " + texto
-            d.metadata["pdf_name"] = pdf.name
+            d.metadata["pdf_name"] = pdf_name
 
         all_docs.extend(docs)
 
     if not all_docs:
-        raise ValueError("Nenhum texto foi extraído. PDF está vazio ou inválido.")
+        raise ValueError("Nenhum texto foi extraído dos PDFs enviados.")
 
     return build_vectorstore(all_docs)
 
